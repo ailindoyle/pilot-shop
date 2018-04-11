27b7b06be56554d32cb9fb191bfe44c85dcddc05
ailindoyle <caitlin.doyle@mobile-life.com>
2018-04-11 12:57:20 +0100

    still not working

--- demo.js
+++ demo.js
@@ -2,14 +2,13 @@ $.get('demo.hbs', function (data) {
     var source = data
     var template = Handlebars.compile(source)
 
-    var data = {
-        item [
-            {name: "name", desc: "description", price: 45, image: "https://cdn.shopify.com/s/files/1/1634/8185/products/akg_aviation_av100_anr_headset_01_270x335_crop_top.png?v=1480672940"}
-
-        ]
+    var dataToUse = {
+        $.get('products.json', function(data) {
+            dataToUse = data
+        })
     }
 
-    var html = template(data)
-    $('main').append(html)
+    var html = template(dataToUse)
+    $('.products').append(html)
 
 })
